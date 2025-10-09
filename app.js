31d859b4487ba617a28ce7164a36f3db77f16e54
bouchardhenry <bouchardhenry@gmail.com>
2025-10-09 16:04:32 +0200

    merge together buttons

--- app.js
+++ app.js
@@ -2,52 +2,6 @@ const fetchGeoQuestions = document.getElementById("category1");
 const fetchMythologyQuestions = document.getElementById("category2");
 const nextQuestion = document.getElementById("next-question");
 
-fetchGeoQuestions.addEventListener("click", () => {
-  fetch(
-    "https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // Visa en fråga i taget
-      let currentQuestionIndex = 0;
-      const questions = data.results;
-
-      if (questions.length > 0) {
-        // Visa första frågan
-        console.log(questions[currentQuestionIndex].question);
-
-      } else {
-        console.log("Inga frågor hittades.");
-      }
-
-      nextQuestion.addEventListener("click", () => {
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.length) {
-          console.log(questions[currentQuestionIndex].question);
-        } else {
-          console.log("Inga fler frågor.");
-        }
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching quiz questions:", error);
-    });
-});
-
-fetchMythologyQuestions.addEventListener("click", () => {
-  fetch(
-    "https://opentdb.com/api.php?amount=5&category=20&difficulty=medium&type=multiple"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-
-    .catch((error) => {
-      console.error("Error fetching quiz questions:", error);
-    });
-});
 // Declare buttons
 const Category1Btn = document.getElementById("category1");
 const Category2Btn = document.getElementById("category2");
@@ -79,10 +33,61 @@ Category2Btn.addEventListener('click', (e)=> {
 startCategory1.addEventListener('click', (e) => {
     category1Section.style.display = 'none';
     questionSection.style.display = 'block';
+    fetch(
+    "https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Visa en fråga i taget
+      let currentQuestionIndex = 0;
+      const questions = data.results;
+
+      if (questions.length > 0) {
+        // Visa första frågan
+        console.log(questions[currentQuestionIndex].question);
+
+      } else {
+        console.log("Inga frågor hittades.");
+      }
+      nextQuestion.addEventListener("click", () => {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+          console.log(questions[currentQuestionIndex].question);
+        } else {
+          console.log("Inga fler frågor.");
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching quiz questions:", error);
+    });
 })
 
 // Kategori 2 -> Questions
 startCategory2.addEventListener('click', (e) => {
     category2Section.style.display = 'none';
     questionSection.style.display = 'block';
-})
+    fetch(
+    "https://opentdb.com/api.php?amount=5&category=20&difficulty=medium&type=multiple"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+
+    .catch((error) => {
+      console.error("Error fetching quiz questions:", error);
+    });
+    nextQuestion.addEventListener("click", () => {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+          console.log(questions[currentQuestionIndex].question);
+        } else {
+          console.log("Inga fler frågor.");
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching quiz questions:", error);
+    });
